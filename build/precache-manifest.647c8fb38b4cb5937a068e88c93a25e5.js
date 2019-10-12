f4fb6638e9e8e7a321e9e220e6478cfa722ca31d
Loviisa Hurme <loviisa.hurme@outlook.com>
2019-10-12 15:46:40 +0300

    Adding front-end build

---
+++ build/precache-manifest.647c8fb38b4cb5937a068e88c93a25e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b272096b5a4952cff6b611977ee2c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a1d06d532432acda34",
+    "url": "/static/css/main.2f48fc30.chunk.css"
+  },
+  {
+    "revision": "a0210ab1b5b72d3108e6",
+    "url": "/static/js/2.09ccb4a8.chunk.js"
+  },
+  {
+    "revision": "77a1d06d532432acda34",
+    "url": "/static/js/main.d0f46251.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
   
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
