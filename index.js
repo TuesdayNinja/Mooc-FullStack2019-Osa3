0894a1cdff4661b6ea7dc2ad731ca75a89269df3
Loviisa Hurme <loviisa.hurme@outlook.com>
2019-10-12 15:34:35 +0300

    Modified for Heroku

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
   
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 
 let persons = [
@@ -95,7 +97,7 @@ let persons = [
 
 
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
